d3624c7d47a7de4acec038f8eb5b9e2bbfde22d3
Vedant Singh <ani.vedant@outlook.com>
2026-05-13 05:14:42 +0530

    feat: add full screen view

--- lib/screens/control_screen.dart
+++ lib/screens/control_screen.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:delta_rover/widgets/control/camera/camera_view.dart';
 import 'package:delta_rover/widgets/control/drive/drive_controls.dart';
 import 'package:delta_rover/widgets/control/servo/servo_control.dart';
 import 'package:delta_rover/widgets/control/sensors/sensor_panel.dart';
 
-class ControlScreen extends StatelessWidget {
+class ControlScreen extends StatefulWidget {
   const ControlScreen({super.key});
+
+  @override
+  State<ControlScreen> createState() => _ControlScreenState();
+}
+
+class _ControlScreenState extends State<ControlScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    SystemChrome.setEnabledSystemUIMode(
+      SystemUiMode.immersiveSticky,
+    );
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setEnabledSystemUIMode(
+      SystemUiMode.edgeToEdge,
+    );
+
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,13 +39,13 @@ class ControlScreen extends StatelessWidget {
           CameraView(),
 
           Padding(
-            padding: EdgeInsets.all(16),
+            padding: EdgeInsets.all(12),
             child: SensorPanel(),
           ),
 
           Expanded(
             child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 36),
+              padding: EdgeInsets.symmetric(horizontal: 3),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
